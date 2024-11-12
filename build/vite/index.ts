f20f01f3b60aaf445ae7e056facc613078758a6f
pegmca <1070826775@qq.com>
2024-11-12 17:54:42 +0800

    🤔 vite配置尝试集成

---
+++ build/vite/index.ts
@@ -0,0 +1,9 @@
+import { createViteServer } from "./server";
+import { createViteBuild } from "./build";
+import { createViteResolve } from "./resolve";
+
+export default {
+  createViteServer,
+  createViteBuild,
+  createViteResolve,
+};
